86372680c06661c363b6fe10821554ba2be18c02
garryk3 <egoruum@gmail.com>
2025-09-29 12:15:04 +0300

    v1.0.5: fix retry plugin

--- src/mf.retry-plugin.js
+++ src/mf.retry-plugin.js
@@ -1,4 +1,6 @@
-import { RetryPlugin } from '@module-federation/retry-plugin';
+import plugin from '@module-federation/retry-plugin';
+
+const RetryPlugin = plugin.RetryPlugin;
 
 /**
  * Creates a retry plugin for module federation with optional custom options.
