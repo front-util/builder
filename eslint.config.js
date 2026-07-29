4f3d958abe509d0cc54405c8dbb52bfa5bf8fa7e
garryk3 <egoruum@gmail.com>
2026-07-29 23:34:09 +0300

    fix: ignore example/ directory in root ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,10 @@
 import { configs } from '@front-utils/linter';
 import { defineConfig } from "eslint/config";
 
-export default defineConfig({
-    files: ['./src/**/*.js'],
-    extends        : configs.js,
-})
+export default defineConfig([
+    { ignores: ['example/**'] },
+    {
+        files: ['./src/**/*.js'],
+        extends: configs.js,
+    },
+])
